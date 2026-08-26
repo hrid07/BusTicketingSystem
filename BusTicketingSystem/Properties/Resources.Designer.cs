3ac1e5ae7cb01335cada35b6a562f0c0056bab63
Arjon Chowdhury <shantachowdhury1969@gmail.com>
2026-08-27 02:39:53 +0600

    signup design updated

--- BusTicketingSystem/Properties/Resources.Designer.cs
+++ BusTicketingSystem/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BusTicketingSystem.Properties
-{
-
-
+namespace BusTicketingSystem.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,134 @@ namespace BusTicketingSystem.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BusTicketingSystem.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap e78a3b4c_0d51_4a1d_9720_cdd7a63978a8 {
+            get {
+                object obj = ResourceManager.GetObject("e78a3b4c-0d51-4a1d-9720-cdd7a63978a8", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Gemini_Generated_Image_ur54fyur54fyur54 {
+            get {
+                object obj = ResourceManager.GetObject("Gemini_Generated_Image_ur54fyur54fyur54", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap image__2_ {
+            get {
+                object obj = ResourceManager.GetObject("image (2)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap image__3_ {
+            get {
+                object obj = ResourceManager.GetObject("image (3)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Untitled_design__5_ {
+            get {
+                object obj = ResourceManager.GetObject("Untitled design (5)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Untitled_design__6_ {
+            get {
+                object obj = ResourceManager.GetObject("Untitled design (6)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Untitled_design2 {
+            get {
+                object obj = ResourceManager.GetObject("Untitled_design2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Untitled_design3 {
+            get {
+                object obj = ResourceManager.GetObject("Untitled_design3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Your_paragraph_text {
+            get {
+                object obj = ResourceManager.GetObject("Your_paragraph_text", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
--- BusTicketingSystem/SignupForm.Designer.cs
+++ BusTicketingSystem/SignupForm.Designer.cs
@@ -47,44 +47,44 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(70, 326);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(93, 401);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(237, 20);
+            this.textBox1.Size = new System.Drawing.Size(315, 22);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(70, 185);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Location = new System.Drawing.Point(93, 228);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(237, 20);
+            this.textBox2.Size = new System.Drawing.Size(315, 22);
             this.textBox2.TabIndex = 1;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(70, 137);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Location = new System.Drawing.Point(93, 169);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(237, 20);
+            this.textBox3.Size = new System.Drawing.Size(315, 22);
             this.textBox3.TabIndex = 2;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(70, 278);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Location = new System.Drawing.Point(93, 342);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(237, 20);
+            this.textBox4.Size = new System.Drawing.Size(315, 22);
             this.textBox4.TabIndex = 3;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(70, 230);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox5.Location = new System.Drawing.Point(93, 283);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(237, 20);
+            this.textBox5.Size = new System.Drawing.Size(315, 22);
             this.textBox5.TabIndex = 4;
             this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
@@ -92,10 +92,10 @@
             // 
             this.labelSignup.AutoSize = true;
             this.labelSignup.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSignup.Location = new System.Drawing.Point(69, 11);
-            this.labelSignup.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSignup.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelSignup.Location = new System.Drawing.Point(92, 14);
             this.labelSignup.Name = "labelSignup";
-            this.labelSignup.Size = new System.Drawing.Size(248, 45);
+            this.labelSignup.Size = new System.Drawing.Size(311, 54);
             this.labelSignup.TabIndex = 5;
             this.labelSignup.Text = "Create Account";
             this.labelSignup.Click += new System.EventHandler(this.label1_Click);
@@ -104,10 +104,10 @@
             // 
             this.labelSub.AutoSize = true;
             this.labelSub.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSub.Location = new System.Drawing.Point(58, 56);
-            this.labelSub.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSub.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelSub.Location = new System.Drawing.Point(78, 69);
             this.labelSub.Name = "labelSub";
-            this.labelSub.Size = new System.Drawing.Size(253, 30);
+            this.labelSub.Size = new System.Drawing.Size(330, 38);
             this.labelSub.TabIndex = 6;
             this.labelSub.Text = "   Sign up to get started  ";
             // 
@@ -115,21 +115,21 @@
             // 
             this.labelSname.AutoSize = true;
             this.labelSname.Font = new System.Drawing.Font("Segoe UI Semibold", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSname.Location = new System.Drawing.Point(72, 109);
-            this.labelSname.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSname.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(199)))), ((int)(((byte)(172)))));
+            this.labelSname.Location = new System.Drawing.Point(96, 134);
             this.labelSname.Name = "labelSname";
-            this.labelSname.Size = new System.Drawing.Size(121, 25);
+            this.labelSname.Size = new System.Drawing.Size(138, 30);
             this.labelSname.TabIndex = 7;
-            this.labelSname.Text = " Full Name    ";
+            this.labelSname.Text = "Full Name    ";
             // 
             // labelSphone
             // 
             this.labelSphone.AutoSize = true;
             this.labelSphone.Font = new System.Drawing.Font("Segoe UI Semibold", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSphone.Location = new System.Drawing.Point(72, 205);
-            this.labelSphone.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSphone.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(199)))), ((int)(((byte)(172)))));
+            this.labelSphone.Location = new System.Drawing.Point(96, 252);
             this.labelSphone.Name = "labelSphone";
-            this.labelSphone.Size = new System.Drawing.Size(65, 25);
+            this.labelSphone.Size = new System.Drawing.Size(77, 30);
             this.labelSphone.TabIndex = 8;
             this.labelSphone.Text = "Phone";
             // 
@@ -137,10 +137,10 @@
             // 
             this.labelSuser.AutoSize = true;
             this.labelSuser.Font = new System.Drawing.Font("Segoe UI Semibold", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSuser.Location = new System.Drawing.Point(72, 156);
-            this.labelSuser.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSuser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(199)))), ((int)(((byte)(172)))));
+            this.labelSuser.Location = new System.Drawing.Point(96, 192);
             this.labelSuser.Name = "labelSuser";
-            this.labelSuser.Size = new System.Drawing.Size(96, 25);
+            this.labelSuser.Size = new System.Drawing.Size(112, 30);
             this.labelSuser.TabIndex = 9;
             this.labelSuser.Text = "Username";
             this.labelSuser.Click += new System.EventHandler(this.label5_Click);
@@ -149,10 +149,10 @@
             // 
             this.labelSpass.AutoSize = true;
             this.labelSpass.Font = new System.Drawing.Font("Segoe UI Semibold", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSpass.Location = new System.Drawing.Point(72, 251);
-            this.labelSpass.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSpass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(199)))), ((int)(((byte)(172)))));
+            this.labelSpass.Location = new System.Drawing.Point(96, 309);
             this.labelSpass.Name = "labelSpass";
-            this.labelSpass.Size = new System.Drawing.Size(90, 25);
+            this.labelSpass.Size = new System.Drawing.Size(105, 30);
             this.labelSpass.TabIndex = 11;
             this.labelSpass.Text = "Password";
             // 
@@ -160,27 +160,28 @@
             // 
             this.labelScpass.AutoSize = true;
             this.labelScpass.Font = new System.Drawing.Font("Segoe UI Semibold", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelScpass.Location = new System.Drawing.Point(72, 299);
-            this.labelScpass.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelScpass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(199)))), ((int)(((byte)(172)))));
+            this.labelScpass.Location = new System.Drawing.Point(96, 368);
             this.labelScpass.Name = "labelScpass";
-            this.labelScpass.Size = new System.Drawing.Size(162, 25);
+            this.labelScpass.Size = new System.Drawing.Size(192, 30);
             this.labelScpass.TabIndex = 12;
             this.labelScpass.Text = "Confirm Password";
             // 
             // buttonSign
             // 
-            this.buttonSign.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonSign.Location = new System.Drawing.Point(131, 385);
-            this.buttonSign.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonSign.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSign.Location = new System.Drawing.Point(175, 475);
+            this.buttonSign.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonSign.Name = "buttonSign";
-            this.buttonSign.Size = new System.Drawing.Size(123, 37);
+            this.buttonSign.Size = new System.Drawing.Size(137, 57);
             this.buttonSign.TabIndex = 13;
             this.buttonSign.Text = "SIGN UP";
             this.buttonSign.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::BusTicketingSystem.Properties.Resources.Untitled_design__6_;
             this.panel1.Controls.Add(this.labelSignup);
             this.panel1.Controls.Add(this.labelSub);
             this.panel1.Controls.Add(this.labelSname);
@@ -194,20 +195,22 @@
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.labelSphone);
             this.panel1.Controls.Add(this.textBox5);
-            this.panel1.Location = new System.Drawing.Point(242, 25);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Location = new System.Drawing.Point(378, 67);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(385, 475);
+            this.panel1.Size = new System.Drawing.Size(498, 585);
             this.panel1.TabIndex = 14;
             // 
             // SignupForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(886, 531);
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = global::BusTicketingSystem.Properties.Resources.Untitled_design3;
+            this.ClientSize = new System.Drawing.Size(1246, 712);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "SignupForm";
             this.Text = "SignUp";
             this.panel1.ResumeLayout(false);
